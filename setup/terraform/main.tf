9b434516f938a36514def96ea42cf1d57085af14
mina <mina@debian.com>
2025-08-04 14:16:57 +0200

    changes bypass val

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_storage_account" "sa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   network_rules {
-    bypass = "AzureServices"
+    bypass = "AzureServices, Metrics"
     default_action="Allow"
   }
 }
